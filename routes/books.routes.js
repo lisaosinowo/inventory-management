543eb8100a210aa81277e004b89763c49d181762
lisaosinowo1 <lisa.osinowo@capgemini.com>
2026-08-05 15:30:37 +0100

    Added the express validators from a separate file to the books routes file

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router(); // this will create a new router object that we can use to define our routes
-const BookModel = require("../models/Book.model")
+const BookModel = require("../models/Book.model");
+const {createBookValidation, updateBookValidation, handleValidationErrors } = require("../validators/book.validator");
 
-router.post("/", async (req, res) => {
-  try {
-    const newBook = await BookModel.create(req.body); // req.body is the body that the user will enter in the postman
-    res.status(201).json(newBook); // this will return the new book that was created in the database as a response to the user
-    // we use .json because we want to return the data in json format
-  } catch (err) {
-    res.status(400).json({ error: err.message }); // this will return the error message as a response to the user if there is an error
-  }
-});
+router.post(
+  "/",createBookValidation, handleValidationErrors, async (req, res) => {
+    try {
+      const newBook = await BookModel.create(req.body); // req.body is the body that the user will enter in the postman
+      res.status(201).json(newBook); // this will return the new book that was created in the database as a response to the user
+      // we use .json because we want to return the data in json format
+    } catch (err) {
+      res.status(400).json({ error: err.message }); // this will return the error message as a response to the user if there is an error
+    }
+  },
+);
 
 router.get("/", async (req, res) => {
   try {
@@ -51,27 +54,34 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-router.put("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updatedBook = await BookModel.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
-    /* findByIdAndUpdate takes in three parameters: 
+router.put(
+  "/:id",
+  updateBookValidation,
+  handleValidationErrors,
+  async (req, res) => {
+    try {
+      const { id } = req.params;
+      const updatedBook = await BookModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      /* findByIdAndUpdate takes in three parameters: 
     1. The id of the book to be updated (id)
     2. The new data to update the book with (req.body)
     3. An options object ({ new: true }) */
 
-    // The { new: true } option tells Mongoose to return the updated document instead of the original document.
+      // The { new: true } option tells Mongoose to return the updated document instead of the original document.
 
-    if (!updatedBook) {
-      return res.status(404).json({ message: "Book not found" });
+      if (!updatedBook) {
+        return res.status(404).json({ message: "Book not found" });
+      }
+
+      res
+        .status(200)
+        .json({ message: "Book updated successfully", updatedBook });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
     }
-
-    res.status(200).json({ message: "Book updated successfully", updatedBook });
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
+  },
+);
 
 module.exports = router; // this will export the router object so that we can use it in other files
