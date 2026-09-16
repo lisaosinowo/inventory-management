bd6dc722a8022ce7350c736e99d37710bd98b7c0
lisaosinowo1 <lisa.osinowo@capgemini.com>
2026-09-16 16:46:17 +0100

    Created some tests for the GET requests

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,6 +1,48 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import BookModel from "../models/Book.model";
 
-test('GET books', async ({ request }) => {
-  const books = await request.get("http://localhost:4000/books");
-  expect(books.ok()).toBeTruthy();
+test.beforeEach(async ({ request }) => {});
+
+test.describe("GET request tests", () => {
+  test("Get all books from database", async ({ request }) => {
+    const response = await request.get("http://localhost:4000/books");
+    expect(response.status()).toBe(200);
+  });
+
+  test("Verify books are in an array and is not empty", async ({ request }) => {
+    const response = await request.get("http://localhost:4000/books");
+    expect(response.status()).toBe(200);
+
+    const data = await response.json();
+    expect(Array.isArray(data)).toBe(true);
+    expect(data.length).toBeGreaterThan(0);
+
+    console.log(data);
+  });
+
+  test("Verify database properties", async ({ request }) => {
+    const response = await request.get("http://localhost:4000/books");
+    const books = await response.json();
+
+    interface Books {
+      bookName: string;
+      countInStock: number;
+      dateCreated: string;
+      image: string;
+      _id: string;
+      id: string;
+      __v: number
+    };
+
+    books.forEach((book: Books) => {
+      expect(book).toHaveProperty("bookName");
+      expect(book).toHaveProperty("countInStock");
+      expect(book).toHaveProperty("dateCreated");
+      expect(book).toHaveProperty("image");
+      expect(book).toHaveProperty("_id");
+      expect(book).toHaveProperty("_id");
+      expect(book).toHaveProperty("__v");
+    });
+    console.log(books);
+  });
 });
