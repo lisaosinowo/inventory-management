64f8ee526331049d76e9eedb8b7416ad408e9e87
lisaosinowo1 <lisa.osinowo@capgemini.com>
2026-09-16 16:45:45 +0100

    Changed the web server URL

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "node index.js",
-    url: "http://localhost:4000",
+    url: "http://localhost:4000/books",
     reuseExistingServer: true,
   },
 });
